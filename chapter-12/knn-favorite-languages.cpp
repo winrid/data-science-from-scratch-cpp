5687054d6fa6490ff4053540f562c8fa435a0217
winrid <winrid@gmail.com>
2020-08-25 23:22:03 -0700

    render with axis

--- chapter-12/knn-favorite-languages.cpp
+++ chapter-12/knn-favorite-languages.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 #include "../common/knn.h"
 
+#define WITHOUT_NUMPY 1
+
+#include "../common/matplotlib.h"
+
+namespace plt = matplotlibcpp;
+
 struct city_favorite_language {
     std::string city;
     double longitude;
@@ -11,20 +17,25 @@ struct city_favorite_language {
 int main() {
 
     std::vector<city_favorite_language> cities = {};
-    cities.push_back((struct city_favorite_language){.city = "Seattle", .longitude = -122.3, .latitude = 47.53, .language = "Python"});
-    cities.push_back((struct city_favorite_language){.city = "Austin", .longitude = -96.85, .latitude = 32.85, .language = "Java"});
-    cities.push_back((struct city_favorite_language){.city = "Madison", .longitude = -89.33, .latitude = 43.13, .language = "R"});
+    cities.push_back({"Seattle", -122.3, 47.53, "Python"});
+    cities.push_back({"Austin", -96.85, 32.85, "Java"});
+    cities.push_back({"Madison", -89.33, 43.13, "R"});
 
     std::unordered_map<std::string, std::string> markers = {
-            { "Java", "o" },
-            { "Python", "s" },
-            { "R", "^" }
+            {"Java",   "o"},
+            {"Python", "s"},
+            {"R",      "^"}
     };
 
     std::unordered_map<std::string, std::string> colors = {
-            { "Java", "r" },
-            { "Python", "b" },
-            { "R", "g" }
+            {"Java",   "r"},
+            {"Python", "b"},
+            {"R",      "g"}
     };
 
+    plt::title("Favorite Programming Languages");
+    plt::xlim(-130, -60);
+    plt::ylim(20, 55);
+    plt::legend();
+    plt::show();
 }
