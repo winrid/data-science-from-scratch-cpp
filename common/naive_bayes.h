abef8a356e4952f75a091cfffc03fe6295f27070
winrid <winrid@gmail.com>
2020-10-13 20:59:41 -0700

    Chapter 13: Naive Bayes - word_probabilities

--- common/naive_bayes.h
+++ common/naive_bayes.h
@@ -10,10 +10,17 @@
 
 namespace naive_bays {
 
-    enum classification {GOOD, BAD};
+    enum classification {
+        GOOD, BAD
+    };
     struct training_item {
         std::string value;
         classification classification;
+    };
+
+    struct training_probability {
+        std::string value;
+        std::map<classification, double> probability_by_classification;
     };
 
     std::set<std::string> tokenize(std::string input) {
@@ -29,13 +36,38 @@ namespace naive_bays {
         return result;
     }
 
-    std::map<std::string, std::map<classification, int>> count_words(const std::vector<training_item>& training_set) {
+    std::map<std::string, std::map<classification, int>> count_words(const std::vector<training_item> &training_set) {
         std::map<std::string, std::map<classification, int>> result;
 
-        for (const training_item& item : training_set) {
-            for (const std::string& word : tokenize(item.value)) {
+        for (const training_item &item : training_set) {
+            for (const std::string &word : tokenize(item.value)) {
                 result[word][item.classification]++;
             }
+        }
+
+        return result;
+    }
+
+    std::vector<training_probability>
+    word_probabilities(std::map<std::string, std::map<classification, int>> counts, double k = 0.5) {
+        std::vector<training_probability> result;
+
+        std::map<classification, int> count_by_classification;
+        for (std::pair<std::string, std::map<naive_bays::classification, int>> word_pair : counts) {
+            for (std::pair<naive_bays::classification, int> classification_pair : word_pair.second) {
+                count_by_classification[classification_pair.first] += classification_pair.second;
+            }
+        }
+
+        for (std::pair<std::string, std::map<naive_bays::classification, int>> word_pair : counts) {
+            std::map<classification, double> probabilities{};
+            for (std::pair<naive_bays::classification, int> classification_pair : word_pair.second) {
+                probabilities[classification_pair.first] = (classification_pair.second + k) / (count_by_classification[classification_pair.first] + 2 * k);
+            }
+            result.push_back({
+                word_pair.first,
+                probabilities
+            });
         }
 
         return result;
